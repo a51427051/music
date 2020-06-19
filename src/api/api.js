78e16a2c8183f4b3ff3c051c3220e90a779cd4e0
a51427051 <1374533941@qq.com>
2020-06-19 18:30:42 +0800

    搜索

--- src/api/api.js
+++ src/api/api.js
@@ -4,3 +4,9 @@ const API_URL = 'http://neteasecloudmusicapi.zhaoboy.com'
 export function getBanner (params) {
   return get(`${API_URL}/banner`, params)
 }
+export function getSearchHot (params) {
+  return get(`${API_URL}/search/hot`, params)
+}
+export function getSearchSuggest (params) {
+  return get(`${API_URL}/search/suggest`, params)
+}
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -3,36 +3,114 @@
     <div class="searchTop">
      <div class="inputStyle">
         <van-icon name="search" />
-        <input type="text" placeholder="请输入歌手或歌曲名" v-model="search" placeholder-style="color:#EDA8A3">
+        <input type="text" placeholder="请输入歌手或歌曲名" v-model="search" placeholder-style="color:#EDA8A3" @input="changeInput" confirm-type="search" @confirm='searchSong'>
         <div>
           <van-icon name="clear" @click="clearInput"/>
         </div>    
      </div>
      <span>取消</span>
     </div>
+
+    <!-- 热门搜索 -->
+    <div class="searchContent searchHot">
+      <p>热门搜索</p>
+      <div class="searchItem">
+        <span v-for="(item,i) in hotList" :key="i">{{item.first}}</span>
+      </div>
+    </div>
+
+    <!-- 搜索历史 -->
+    <div class="searchContent searchHistory">
+      <p>热门搜索</p>
+      <van-icon name="clear" @click="clearHistory" size="20px"/>
+      <div class="searchItem">
+        <span v-for="(item,i) in history" :key="i">{{item}}</span>
+      </div>
+    </div>
+
+    <!-- 搜索建议 -->
+    <div class="searchSuggest" v-if="searchSuggest">
+      <p v-for="(item,i) in suggestList" :key="i">
+        {{item.keyword}}
+      </p>
+    </div>
+
   </div>
 </template>
 
 <script>
+import {getSearchHot, getSearchSuggest, getSearchList} from '@/api/api'
 export default {
   data () {
     return {
-      search: 'aaa'
+      hotList: [],
+      search: '',
+      suggestList: [],
+      searchSuggest: false,
+      history: wx.getStorageSync('history') || []
     }
   },
   onLoad () {
-
+    this.getSearchHot()
   },
   methods: {
     clearInput () {
       console.log(this.search)
       this.search = ''
+      this.suggestList = []
+      this.searchSuggest = false
+    },
+    getSearchHot () {
+      getSearchHot({ type: 'news' })
+        .then(response => {
+          this.hotList = response.data.result.hots
+        })
+    },
+    // 搜索建议
+    changeInput (e) {
+      var suggest = e.mp.detail.value
+      if (suggest !== '') {
+        getSearchSuggest({ type: 'mobile', keywords: suggest })
+          .then(response => {
+            if (JSON.stringify(response.data.result) === '{}') {
+              this.suggestList = []
+              this.searchSuggest = false
+            }
+            this.suggestList = response.data.result.allMatch
+            this.searchSuggest = this.suggestList.length > 0 ? 'true' : 'false'
+          })
+      } else {
+        this.searchSuggest = false
+        this.suggestList = []
+      }
+    },
+    // 清空搜索历史
+    clearHistory () {
+      var that = this
+      wx.removeStorage({
+        key: 'history',
+        success (res) {
+          that.history = []
+        }
+      })
+    },
+    // 搜索歌曲
+    searchSong () {
+      this.history.push(this.search)
+      wx.setStorage({
+        key: 'history',
+        data: this.history
+      })
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+.contain{
+  position: relative;
+
+}
 .searchTop{
   background: #e3453a;
   width: 100%;
@@ -68,6 +146,49 @@ export default {
   }
   span{
     margin-left: 10px;
+  }
+}
+.searchContent{
+  padding: 20px;
+  position: relative;
+  p{
+    color: #e3453a;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .searchItem{
+    span{
+      padding: 0 5px;
+      color: #e3453a;
+      background: #1111;
+      margin: 0 10px 10px 0;
+      border-radius: 4px;
+      line-height: 28px;
+      height: 28px;
+      display: inline-block;
+    }
+  }
+  van-icon{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    color: #e3453a;
+  }
+}
+.searchSuggest{
+  position: absolute;
+  background: #ffffff;
+  top: 50px;
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+  box-sizing: border-box;
+  p{
+    padding: 0 10px;
+    line-height: 40px;
+    height: 40px;
+    border-bottom: 1px solid #eaeaea;
   }
 }
 </style>
